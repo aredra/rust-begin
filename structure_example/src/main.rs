45fe71c2d4d73262e9f7b1a922dd6e258aea0e17
aredra <rnswn48@gmail.com>
2022-03-16 00:24:15 +0900

    fixed: Typo semi colon

--- structure_example/src/main.rs
+++ structure_example/src/main.rs
@@ -30,7 +30,7 @@ fn main() {
     let rect3 = Rectangle { length: 45, width: 60 };
 
     println!("The area of the rectangle is {} square pixels.", rect1.area());
-    println!("rect1 is {:?}", rect1)
+    println!("rect1 is {:?}", rect1);
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
